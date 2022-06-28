0b209f6faae1b6df8ea8aa269a11bca3f6ccb7ca
coo <46950239+onion09@users.noreply.github.com>
2022-06-28 14:25:34 -0700

    test push

--- MyToDo/Views/MainPage.xaml.cs
+++ MyToDo/Views/MainPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MyToDo.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +17,28 @@ namespace MyToDo.Views
         public MainPage()
         {
             InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            var todos = new List<ToDo>();
+            var files = Directory.EnumerateFiles(Environment.GetFolderPath(
+            Environment.SpecialFolder.LocalApplicationData), "*.notes.txt");
+            foreach(var file in files)
+            {
+                var todo = new ToDo
+                {
+                    Text = File.ReadAllText(file),
+                    FileName = file,
+                    Date = File.GetCreationTime(file)
+                };
+                todos.Add(todo);
+            }
+            TodoListView.ItemsSource = todos.OrderByDescending(t => t.Date);
+        }
+
+        private void TodoListView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
         }
     }
 }
